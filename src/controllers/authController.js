4c3564efec345a60b3ce3b3341aa0b8a6c2516fb
Ukpabi Godwin <gwindotcool@gmail.com>
2026-07-31 06:25:21 -0500

    feat: implement login with JWT

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+//Register User
 exports.register = async (req, res) => {
     try {
         const { fullName, email, password, role } = req.body;
@@ -29,6 +31,66 @@ exports.register = async (req, res) => {
         res.status(201).json({
             success: true,
             message: "User registered successfully",
+            user: {
+                id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+//login User
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Find user by email
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password"
+            });
+        }
+
+        // Compare entered password with hashed password
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isPasswordCorrect) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRES || "1d"
+            }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            token: token,
             user: {
                 id: user._id,
                 fullName: user.fullName,
